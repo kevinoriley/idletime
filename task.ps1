f88c647a9b6f0199137d2f26f8853b9c7d23f81c
kevinoriley <109296879+kevinoriley@users.noreply.github.com>
2022-07-14 12:34:25 -0400

    Update task.ps1

--- task.ps1
+++ task.ps1
@@ -1,8 +1,9 @@
+# Set variables
 $taskName = "IdleTime"
 $folder = "C:\Users\Public\PublicScripts"
 $repository = "https://methodgrouppublic.blob.core.windows.net/scripts"
 
-# Create files
+# Create folder and files, if necessary
 If (!(Test-Path -PathType container $folder))
 {
 	New-Item -ItemType Directory -Path $folder
@@ -18,10 +19,10 @@ If (-not(Test-Path -Path $folder\idlecheck.ps1))
 Invoke-WebRequest -Uri $repository/idlecheck.ps1 -OutFile $folder\idlecheck.ps1
 }
 
-# Delete task
+# Delete old task
 Unregister-ScheduledTask -TaskName $taskName -Confirm:$false -ErrorAction Ignore
 
-# Create task
+# Create new task
 $action = New-ScheduledTaskAction -Execute "wscript.exe" -Argument "$folder\psrun.vbs $folder\idlecheck.ps1"
 $trigger = New-ScheduledTaskTrigger -Once -At 12am -RepetitionDuration  (New-TimeSpan -Days 1)  -RepetitionInterval  (New-TimeSpan -Minutes 5)
 $principal = New-ScheduledTaskPrincipal -UserId (Get-CimInstance –ClassName Win32_ComputerSystem | Select-Object -expand UserName)
